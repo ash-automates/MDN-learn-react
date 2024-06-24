a0ed12b175db799c8995d2303f11459fa88f22ee
Hachem <s.hachem@murena.io>
2024-06-24 16:09:15 +0100

    Implemented full version of the task filtering feature

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,25 @@ import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 
+const FILTER_MAP = {
+  All: () => {
+    return true;
+  },
+  Active: (task) => {
+    return !task.completed;
+  },
+  Completed: (task) => {
+    return task.completed;
+  },
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState("All");
 
-  const tasksList = tasks?.map((task) => {
+  const tasksList = tasks?.filter(FILTER_MAP[filter]).map((task) => {
     return (
       <Todo
         id={task.id}
@@ -17,6 +32,17 @@ function App(props) {
         toggleTaskCompleted={toggleTaskCompleted}
         deleteTask={deleteTask}
         editTask={editTask}
+      />
+    );
+  });
+
+  const filtersList = FILTER_NAMES.map((name) => {
+    return (
+      <FilterButton
+        key={name}
+        name={name}
+        isPressed={filter === name}
+        setFilter={setFilter}
       />
     );
   });
@@ -50,9 +76,9 @@ function App(props) {
     setTasks(editedTaskList);
   }
 
-  const tasksNoun = tasks.length !== 1 ? "tasks" : "task";
+  const tasksNoun = tasksList.length !== 1 ? "tasks" : "task";
 
-  const headingText = `${tasks.length} ${tasksNoun} remaining`;
+  const headingText = `${tasksList.length} ${tasksNoun} remaining`;
 
   function addTask(name) {
     const newTask = { id: `todo-${nanoid()}`, name, completed: false };
@@ -63,11 +89,7 @@ function App(props) {
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
       <Form addTask={addTask} />
-      <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
-      </div>
+      <div className="filters btn-group stack-exception">{filtersList}</div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
